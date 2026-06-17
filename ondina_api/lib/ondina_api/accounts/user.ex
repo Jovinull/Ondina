5f5c7bd9087b694855077a7fe1bf36238d0a48bf
Jovinull <felipejovinogamerplay@gmail.com>
2026-06-17 14:42:43 -0300

    feat(api): criar modelo de usuário e migração para contas

---
+++ ondina_api/lib/ondina_api/accounts/user.ex
@@ -0,0 +1,30 @@
+defmodule OndinaApi.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :password_hash, :string
+    field :username, :string
+    field :password, :string, virtual: true
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email, :password])
+    |> validate_required([:username, :email, :password])
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case get_change(changeset, :password) do
+      nil -> changeset
+      password -> put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
+    end
+  end
+end
